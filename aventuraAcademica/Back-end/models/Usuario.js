fbf9ec8aafbb751c478d54f94d357fb5a0e20a06
Bruno Carvalho da Silva <bruno.silva@lsdi.ufma.br>
2024-06-19 23:21:14 -0300

    Criando API dos usuários

---
+++ aventuraAcademica/Back-end/models/Usuario.js
@@ -0,0 +1,25 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+const User = sequelize.define('User', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    senha: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: true
+});
+
+export default User;
